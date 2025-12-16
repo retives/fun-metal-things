efc5445e7833cef04e43475d75cf7bb7204c4b61
srgtkrvvv <tr3e1t0ry@gmail.com>
2025-12-16 17:56:15 +0200

    Add image processor

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,18 @@ require_relative "boot"
 
 require "rails/all"
 
-if ['development', 'test'].include? ENV['RAILS_ENV']
-  require 'dotenv-rails'
-  Dotenv::Railtie.load
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module FunMetalThings
   class Application < Rails::Application
     config.load_defaults 8.0
+
+    config.active_storage.variant_processor = :vips
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     config.active_record.schema_format = :sql
     config.generators do |g|

2a9dbc908f51ae699bb19166fcbd87abf71c4dc8
srgtkrvvv <tr3e1t0ry@gmail.com>
2025-12-17 21:23:57 +0200

    update deploy script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# Ця команда оновить схему PostgreSQL на Render
 bundle exec rails db:migrate
-bundle exec rails db:seed

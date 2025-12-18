a22882681ab0018705f7aafd58cb32a51458a685
srgtkrvvv <tr3e1t0ry@gmail.com>
2025-12-18 19:12:40 +0200

    Change _item styles

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module FunMetalThings
     config.load_defaults 8.0
 
     config.active_storage.variant_processor = :vips
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     config.active_record.schema_format = :sql
     config.generators do |g|
